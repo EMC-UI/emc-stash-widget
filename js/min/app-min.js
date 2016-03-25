7313f8e76be8617aa828106f579b5a4864d31a4f
zherring <z.herring@gmail.com>
2016-03-25 10:53:19 -0600

    project output

---
+++ js/min/app-min.js
@@ -0,0 +1,1 @@
+!function(o,t){"use strict";o.module("app",[]).controller("appController",["$scope",function(o){o.topProjects=[{name:"Project1",commits:524,contrib:2},{name:"project2",commits:324,contrib:5},{name:"Project3",commits:142,contrib:1},{name:"Project4",commits:36,contrib:10},{name:"Project5",commits:12,contrib:2}]}])}(window.angular,window._);
