4d9b1547fa070a3110eb610f444efd557b03aafa
zherring <z.herring@gmail.com>
2016-03-25 14:42:53 -0600

    switching works!

--- js/app.js
+++ js/app.js
@@ -2,7 +2,14 @@
 	'use strict';
 
 	$angular.module('app', [])
-    .controller('appController', ['$scope', '$http', function($scope,$http) {
+    .run(['$rootScope', "$interval", function($rootScope, $interval) {
+        $rootScope.view = 1;
+        $interval(function() {
+             $rootScope.view = ($rootScope.view === 1 ? 2 : 1);
+             console.log($rootScope.view);
+        }, 5000)
+    }])
+    .controller('projController', ['$scope', '$http', function($scope,$http) {
 
         // for Compass/SCSS stuff
         $scope.topProjects = [
@@ -21,7 +28,6 @@
 
 
         // Grab percentage of contribution
-        var percentage = [];
         $scope.topProjects.forEach (function(proj){
             var percent = Math.round((proj.commits / $scope.topProjects[0].commits) * 100);
             proj.percent = percent;
@@ -29,7 +35,34 @@
 
         )
 
-        console.log(percentage);
+
+    }])
+
+    .controller('contribController', ['$scope', '$http', function($scope,$http) {
+
+        // for Compass/SCSS stuff
+        $scope.topContributors = [
+            {name: "Peter Parker", commits: 35, proj: 2},
+            {name: "Felica Hardy", commits: 23, proj: 5},
+            {name: "Mary Jane Watson", commits: 12, proj: 1},
+            {name: "J. Jonah Jameson", commits: 11, proj: 10},
+            {name: "Phil Urich", commits: 8, proj: 2},
+        ];
+
+
+        // Expressjs Magic
+        // $http.get("/stats/repos").then(function(response){
+        //     $scope.topProjects = response.data;
+        // });
+
+
+        // Grab percentage of contribution
+        $scope.topContributors.forEach (function(contrib){
+            var percent = Math.round((contrib.commits / $scope.topContributors[0].commits) * 100);
+            contrib.percent = percent;
+            }
+
+        )
 
     }]);
 
