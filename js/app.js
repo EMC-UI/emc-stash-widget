2f0d2a0e64c543aa22fef4541fe3b45a378ad307
zherring <z.herring@gmail.com>
2016-03-25 12:04:27 -0600

    percentage is in!

--- js/app.js
+++ js/app.js
@@ -2,8 +2,9 @@
 	'use strict';
 
 	$angular.module('app', [])
-    .controller('appController', ['$scope', function($scope) {
+    .controller('appController', ['$scope', '$http', function($scope,$http) {
 
+        // for Compass/SCSS stuff
         $scope.topProjects = [
             {name: "Project1", commits: 524, contrib: 2},
             {name: "project2", commits: 324, contrib: 5},
@@ -12,6 +13,22 @@
             {name: "Project5", commits: 12, contrib: 2},
         ];
 
+
+        // Expressjs Magic
+        // $http.get("/stats/repos").then(function(response){
+        //     $scope.topProjects = response.data;
+        // });
+
+
+        var percentage = [];
+        $scope.topProjects.forEach (function(proj){
+            var percent = Math.round((proj.commits / $scope.topProjects[0].commits) * 100);
+            proj.percent = percent;
+            }
+
+        )
+
+        console.log(percentage);
 
     }]);
 
